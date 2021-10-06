8de9c0ed59b287d496ab4e589871fc09f16552db
gilbertomoj <gibamedeirosgc@gmail.com>
2021-10-05 21:48:48 -0300

    Atividades

---
+++ 1/index.js
@@ -0,0 +1,46 @@
+// 1)      Criar uma função anônima que recebe duas notas de um aluno do ensino médio:
+
+// a)       Calcular a média do aluno.
+
+// b)      Se a média for maior ou igual a 7, retornar "Aprovado!";
+
+// c)       Se a média for maior ou igual a 5 e menor que 7, retornar "Recuperação!";
+
+// d)      Se a média for menor que 5, retornar "Reprovado!".
+
+// e)      Chamar a função e exibir no console o resultado.
+// let calcMedia = (n1 , n2) => {
+//     var media = (n1 + n2 )/2
+//     if(media >= 7){
+//         console.log("Aprovado")
+//     }else if(media < 7 || media >= 5){
+//         console.log("Recuperação")
+//     }else{
+//         console.log("Reprovado")
+//     }
+// }
+// calcMedia(7,1) 
+
+// 2)      Criar uma função anônima que calcula a tabuada de 3.
+
+// a)       Chamar a função e exibir no console o resultado.
+// let tab3 = () =>{
+//     for(var i = 1; i < 11 ; i ++){
+//         console.log(3*i)
+//     }
+// }
+// tab3()
+
+// 3)      Criar uma função de seta (arrow function) que recebe o ano de nascimento de uma pessoa e retorne se ela é maior de idade ou menor.
+
+// a)       Chamar a função e exibir no console o resultado.
+// let datanasc = (ano) => {
+//     var anoatual = 2021
+//     var idade = anoatual - ano
+//     if(idade >= 18){
+//         console.log("É de maior")
+//     }else{
+//         console.log("É de menor")
+//     }
+// }
+// datanasc(2001)
